723ff8aaaefaea21921c515472e90264a9c210bc
Gmcommunications2020 <Gmcommunications2020>
2022-12-08 11:16:52 -0800

    Print words

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * Return: Always 0 (sucess)
- * */
-
+ * Return: o
+ */
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
