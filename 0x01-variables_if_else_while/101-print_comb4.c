4901725513977e324144d2cd53727d1e25301fe6
Gmcommunications2020 <Gmcommunications2020>
2022-12-09 06:30:47 -0800

    101-print_combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,7 @@ int ones = '0';
 int tens = '0';
 int hundreds = '0';
 
-for  (hundreds = '0'; hundreds <= '9'; hundreds++)
+for (hundreds = '0'; hundreds <= '9'; hundreds++)
 {
 for (tens = '0'; tens <= '9'; tens++)
 {
@@ -30,5 +30,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return(0);
+return (0);
 }
