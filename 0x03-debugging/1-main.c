62e5a81c6e5980987c3c140f2bda6ef72e3ff57f
Gmcommunications2020 <Gmcommunications2020>
2022-12-15 04:37:33 -0800

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,10 +12,13 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	print("infinite loop avoided! \\o/\n");
 
