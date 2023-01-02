529118748406aeadbc2a1e94da221d3915340227
Gmcommunications2020 <Gmcommunications2020>
2023-01-02 03:38:43 -0800

    07 pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,6 @@
 
 char *memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
 char *_memcpy(cahr *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
