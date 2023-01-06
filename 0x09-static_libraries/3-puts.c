d975a996900a1dc3f34a90a04100e6c5f6ce7048
Gmcommunications2020 <Gmcommunications2020>
2023-01-06 04:05:20 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+_puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
